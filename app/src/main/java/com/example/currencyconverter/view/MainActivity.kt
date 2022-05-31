c4fa6ffb61c3d0c72808bf5bedff40e97ba20208
Hei Lok Keith Kong <keithkong1996@gmail.com>
2022-05-31 13:53:09 +0800

    Set up Retrofit, Add utility & help classes

--- app/src/main/java/com/example/currencyconverter/view/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/view/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.currencyconverter.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.currencyconverter.R
-import com.example.currencyconverter
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
